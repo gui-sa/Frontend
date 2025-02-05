6218c33a378b7910a93284477aa6dde1b3745478
gui-sa <guime.sa9@gmail.com>
2025-02-05 15:36:59 -0300

    Adiciona UseRef e resolve o BO de sincronicidade

--- ReactCourse2/learn.md
+++ ReactCourse2/learn.md
@@ -12,4 +12,19 @@
 
  Quando se deseja algo dinâmico, ou seja, MUTÁVEL.
  Quando a variável se altera: ele gera um update das variáveis dentro da página.
+ A ação de setar algo é ASSINCRONA! mais que isso ela não é uma promise ou tratada como uma função assincrona!... Para capturar esses casos: use o USEEFECT JUNTO!
  
+## useEfect:
+
+ Ele é um side efect, ou seja, opera logo após a montagem não impactando diretamente na montagem inicial da pagina.
+
+ Curiosidade: ao testar em ambiente de desenvolvimento: os plots de montagem virão duplicados e isso se deve ao RESTRICT MODE que monta para testar e remonta para voce.
+
+ useEffect (()=>{//executa somente uma vez ao carregar a pagina},[])
+ useEffect (()=>{//executa somente uma vez ao carregar a pagina e sempre que a variavel YEAH for alterada},[YEAH])
+
+ Não existe uma ordem de qual useEfect rodar primeiro...
+
+## useRef:
+
+ The useRef Hook Permite criar PERSISTÊNCIAS em variáveis entre as renderizações. Diferente do useState alterá-las não gera outra renderização.
--- ReactCourse2/lista-de-compras-simples/src/App.tsx
+++ ReactCourse2/lista-de-compras-simples/src/App.tsx
@@ -1,15 +1,34 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import style from "./App.module.css";
 
 function App() {
   const [todo, setTodo] = useState<string[]>([])
   const [newTask, setNewTask] = useState<string>("");
+  const firstRender = useRef(true);
+
+  useEffect(()=>{
+    const localtodo = localStorage.getItem("@localtodo");
+    if(localtodo){
+      const prevTasks = JSON.parse(localtodo);
+      setTodo(prevTasks);
+    }
+  },[]);
+
+
+  useEffect(()=>{
+    if(firstRender.current){
+      firstRender.current=false;
+      return
+    }
+    localStorage.setItem("@localtodo",JSON.stringify(todo));
+  },[todo]);
+
 
   function reset(){
     setNewTask("");
   }
 
-  function criarTarefa():void{
+   function criarTarefa():void{
     if(newTask===""){
       alert("Por favor, digite uma descrição para sua tarefa");
       return;
@@ -22,7 +41,7 @@ function App() {
     if(todo[index]){
       const temp = [...todo];
       temp.splice(index,1);
-      setTodo([...temp]);
+      setTodo(temp);
     }
   }
 
@@ -30,8 +49,8 @@ function App() {
     <div>
         <h1>TODO</h1>
         <hr></hr>
-        <form>
-          <label onSubmit={(e)=>{e.preventDefault()}}>
+        <form onSubmit={(e)=>{e.preventDefault()}}>
+          <label >
             <input 
               type="text" 
               name="newtask"  
